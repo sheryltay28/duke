daec1aa1807c329b1e70ca0d482efb9eab5714eb
Sheryl Tay <sheryltayyyy@gmail.com>
2019-08-22 01:07:11 +0800

    level 4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -25,12 +25,43 @@ public class Duke {
                 done.doTask();
                 System.out.println("Nice! I've marked this task as done: ");
                 System.out.println(done);
-
-
-           } else if (!input.equals("bye")) {
-                inputs.add(task);
-                System.out.println("added: " + input);
-            } else {
+           } else if (line[0].equals("todo")) {
+                String todoTask = line[1];
+                for (int i = 2; i < line.length; i++) {
+                    todoTask += " " + line[i];
+                }
+                Todo newTodo = new Todo(todoTask);
+                inputs.add(newTodo);
+                System.out.println("Got it. I've added this task:");
+                System.out.println(newTodo);
+                System.out.println("Now you have " + inputs.size() + " tasks in the list.");
+            } else if (line[0].equals("deadline")) {
+                String findDeadline[] = input.split("/");
+                String by = findDeadline[1].substring(3);
+                String findTask[] = findDeadline[0].split(" ");
+                String deadlineTask = findTask[1];
+                for (int i = 2; i <findTask.length; i++) {
+                    deadlineTask += " " + findTask[i];
+                }
+                Deadline deadline = new Deadline(deadlineTask, by);
+                inputs.add(deadline);
+                System.out.println("Got it. I've added this task:");
+                System.out.println(deadline);
+                System.out.println("Now you have " + inputs.size() + " tasks in the list.");
+            } else if (line[0].equals("event")) {
+                String findTime[] = input.split("/");
+                String at = findTime[1].substring(3);
+                String findTask[] = findTime[0].split(" ");
+                String eventTask = findTask[1];
+                for (int i = 2; i < findTask.length; i++) {
+                    eventTask += " " + findTask[i];
+                }
+                Event event = new Event(eventTask, at);
+                inputs.add(event);
+                System.out.println("Got it. I've added this task:");
+                System.out.println(event);
+                System.out.println("Now you have " + inputs.size() + " tasks in the list.");
+            } else if (input.equals("bye")) {
                break;
             }
         }
@@ -54,10 +85,49 @@ class Task {
 
     @Override
     public String toString() {
-        if (done == true) {
+        if (done) {
             return "[✓] " + task;
         } else {
             return "[✗] " + task;
         }
+    }
+}
+
+class Todo extends Task {
+    public Todo(String task) {
+        super(task);
+    }
+
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+}
+
+class Deadline extends Task {
+    String by;
+
+    public Deadline(String task, String by) {
+        super(task);
+        this.by = by;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + by + ")";
+    }
+}
+
+class Event extends Task {
+    String at;
+
+    public Event(String task, String at) {
+        super(task);
+        this.at = at;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (at: " + at + ")";
     }
 }
