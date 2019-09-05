8f0a7ac95ba94ccd99bada14969df39111e8dc6f
Sheryl Tay <sheryltayyyy@gmail.com>
2019-09-05 23:47:59 +0800

    A-JUnit

---
+++ src/test/java/StorageTest.java
@@ -0,0 +1,50 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class StorageTest {
+    Storage storage;
+
+    @Test
+    public void testGetFilePath() {
+        storage = new Storage("/Users/sheryl/CS2103/duke/src/main/java/Storage.java");
+        assertEquals("/Users/sheryl/CS2103/duke/src/main/java/Storage.java", storage.getFilePath());
+    }
+
+    @Test
+    public void testConvertToInt() {
+        assertEquals(1, storage.convertToInt("February"));
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
